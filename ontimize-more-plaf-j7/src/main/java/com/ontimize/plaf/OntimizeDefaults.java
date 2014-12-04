f09aa7e531c9665d05a370c8690e853d7da551d9
daniel.grana <daniel.grana@imatia.com>
2014-12-04 11:45:42 +0000

    Sonar compliance formatting code.
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-more-plaf/trunk@1755 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

--- ontimize-more-plaf-j7/src/main/java/com/ontimize/plaf/OntimizeDefaults.java
+++ ontimize-more-plaf-j7/src/main/java/com/ontimize/plaf/OntimizeDefaults.java
@@ -6,9 +6,10 @@ import java.net.URL;
 import javax.swing.UIDefaults;
 
 import com.ontimize.plaf.painter.AbstractRegionPainter.PaintContext;
+
 public class OntimizeDefaults {
 
-	public static class LazyPainter implements UIDefaults.LazyValue{
+	public static class LazyPainter implements UIDefaults.LazyValue {
 
 		protected int which;
 		protected String className;
@@ -32,7 +33,7 @@ public class OntimizeDefaults {
 			this.which = which;
 			this.ctx = ctx;
 		}
-		
+
 		public LazyPainter(String className, int which, PaintContext ctx, String path) {
 			this.className = className;
 			this.which = which;
@@ -47,71 +48,58 @@ public class OntimizeDefaults {
 				Object cl;
 
 				// See if we should use a separate ClassLoader
-				if (table == null || !((cl = table.get("ClassLoader"))
-						instanceof ClassLoader)) {
-					cl = Thread.currentThread().
-					getContextClassLoader();
+				if (table == null || !((cl = table.get("ClassLoader")) instanceof ClassLoader)) {
+					cl = Thread.currentThread().getContextClassLoader();
 					if (cl == null) {
 						// Fallback to the system class loader.
 						cl = ClassLoader.getSystemClassLoader();
 					}
 				}
 
-
 				// CALLING THE CLASS BY REFLEXION:
 				Constructor constructor = null;
 
-				// to call methods, whose construction has two parameters: an int and an URL:
-				if (path!=null && ctx != null){
-					c = Class.forName(className, true, (ClassLoader)cl);
-					constructor = c.getConstructor(int.class,PaintContext.class,URL.class);
+				// to call methods, whose construction has two parameters: an
+				// int and an URL:
+				if (path != null && ctx != null) {
+					c = Class.forName(className, true, (ClassLoader) cl);
+					constructor = c.getConstructor(int.class, PaintContext.class, URL.class);
 					if (constructor == null) {
-						throw new NullPointerException(
-								"Failed to find the constructor for the class: " +
-								className);
+						throw new NullPointerException("Failed to find the constructor for the class: " + className);
 					}
-					return constructor.newInstance(which,ctx,getClass().getClassLoader().getResource(this.path));
+					return constructor.newInstance(which, ctx, getClass().getClassLoader().getResource(this.path));
 
-					
-				}else if (path!=null){
-					c = Class.forName(className, true, (ClassLoader)cl);
-					constructor = c.getConstructor(int.class,URL.class);
+				} else if (path != null) {
+					c = Class.forName(className, true, (ClassLoader) cl);
+					constructor = c.getConstructor(int.class, URL.class);
 					if (constructor == null) {
-						throw new NullPointerException(
-								"Failed to find the constructor for the class: " +
-								className);
+						throw new NullPointerException("Failed to find the constructor for the class: " + className);
 					}
-					return constructor.newInstance(which,getClass().getClassLoader().getResource(this.path));
+					return constructor.newInstance(which, getClass().getClassLoader().getResource(this.path));
 
-
-					// to call methods, whose construction has two parameters: an int and an PaintContext:
-				} else if (ctx!=null){
-					c = Class.forName(className, true, (ClassLoader)cl);
-					Class paintContextClass = Class.forName("com.ontimize.plaf.painter.AbstractRegionPainter$PaintContext", true, (ClassLoader)cl);
-					constructor = c.getConstructor(int.class,paintContextClass);
-//							PaintContext.class);
+					// to call methods, whose construction has two parameters:
+					// an int and an PaintContext:
+				} else if (ctx != null) {
+					c = Class.forName(className, true, (ClassLoader) cl);
+					Class paintContextClass = Class.forName("com.ontimize.plaf.painter.AbstractRegionPainter$PaintContext", true, (ClassLoader) cl);
+					constructor = c.getConstructor(int.class, paintContextClass);
+					// PaintContext.class);
 					if (constructor == null) {
-						throw new NullPointerException(
-								"Failed to find the constructor for the class: " +
-								className);
+						throw new NullPointerException("Failed to find the constructor for the class: " + className);
 					}
 					return constructor.newInstance(which, ctx);
 
+					// to call methods, whose construction has two parameters:
+					// an int and an URL:
+				} else {
 
-					// to call methods, whose construction has two parameters: an int and an URL:
-				}else{
-
-					c = Class.forName(className, true, (ClassLoader)cl);
+					c = Class.forName(className, true, (ClassLoader) cl);
 					constructor = c.getConstructor(int.class);
 					if (constructor == null) {
-						throw new NullPointerException(
-								"Failed to find the constructor for the class: " +
-								className);
+						throw new NullPointerException("Failed to find the constructor for the class: " + className);
 					}
 					return constructor.newInstance(which);
 				}
-
-
 
 			} catch (Exception e) {
 				e.printStackTrace();
