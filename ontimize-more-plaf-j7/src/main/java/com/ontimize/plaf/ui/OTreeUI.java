9c756e210e56ad7b19ae6784b4947a87632efdd2
daniel.grana <daniel.grana@imatia.com>
2014-12-05 13:41:23 +0000

    Bug fixing. Solved problem of ticket 776624.
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-more-plaf/trunk@1774 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

--- ontimize-more-plaf-j7/src/main/java/com/ontimize/plaf/ui/OTreeUI.java
+++ ontimize-more-plaf-j7/src/main/java/com/ontimize/plaf/ui/OTreeUI.java
@@ -18,7 +18,6 @@ import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicTreeUI;
 import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
@@ -36,31 +35,30 @@ public class OTreeUI extends BasicTreeUI {
 		return new OTreeUI();
 	}
 
-	public OTreeUI() {
-	}
+	public OTreeUI() {}
 
 	@Override
 	protected void setSelectionModel(TreeSelectionModel newLSM) {
 		super.setSelectionModel(newLSM);
 		if (this.treeSelectionModel != null) {
-			this.treeSelectionModel
-					.addTreeSelectionListener(new TreeSelectionListener() {
+			this.treeSelectionModel.addTreeSelectionListener(new TreeSelectionListener() {
 
-						@Override
-						public void valueChanged(TreeSelectionEvent e) {
-							if (tree != null)
-								tree.repaint();
-						}
-					});
+				@Override
+				public void valueChanged(TreeSelectionEvent e) {
+					if (OTreeUI.this.tree != null) {
+						OTreeUI.this.tree.repaint();
+					}
+				}
+			});
 		}
 	}
 
 	@Override
 	protected void installDefaults() {
 		super.installDefaults();
-		lineTypeCurved = UIManager.getBoolean("Tree.lineTypeCurved");
-		lineColor = UIManager.getColor("Tree.lineColor");
-		lineSelectionColor = UIManager.getColor("Tree.lineSelectionColor");
+		this.lineTypeCurved = UIManager.getBoolean("Tree.lineTypeCurved");
+		this.lineColor = UIManager.getColor("Tree.lineColor");
+		this.lineSelectionColor = UIManager.getColor("Tree.lineSelectionColor");
 
 	}
 
@@ -70,70 +68,86 @@ public class OTreeUI extends BasicTreeUI {
 	}
 
 	protected TreePath getLeadSelectionPath() {
-		return tree.getLeadSelectionPath();
+		return this.tree.getLeadSelectionPath();
 	}
 
 	protected void updateLeadRow() {
-		leadRow = getRowForPath(tree, getLeadSelectionPath());
+		this.leadRow = this.getRowForPath(this.tree, this.getLeadSelectionPath());
 	}
 
+	@Override
 	protected int getLeadSelectionRow() {
-		return leadRow;
+		return this.leadRow;
 	}
 
 	@Override
 	public void paint(Graphics g, JComponent c) {
-		updateLeadRow();
+		this.updateLeadRow();
 		super.paint(g, c);
-		
-		//Paint selected lines
-		Rectangle        paintBounds = g.getClipBounds();
-		Insets           insets = tree.getInsets();
-		
-		TreePath         initialPath = tree.getSelectionPath();//getClosestPathForLocation(tree, 0, paintBounds.y);
-		
-		if (initialPath!=null){
-			TreePath   parentPath = initialPath;
-//			parentPath = parentPath.getParentPath();
-			while(parentPath != null) {
-				paintVerticalSelectedPart(g, paintBounds, insets, parentPath);
-				drawingCache.put(parentPath, Boolean.TRUE);
+
+		// Paint selected lines
+		Rectangle paintBounds = g.getClipBounds();
+		Insets insets = this.tree.getInsets();
+
+		TreePath initialPath = this.tree.getSelectionPath();// getClosestPathForLocation(tree,
+		// 0,
+		// paintBounds.y);
+
+		if (initialPath != null) {
+			TreePath parentPath = initialPath;
+			// parentPath = parentPath.getParentPath();
+			while (parentPath != null) {
+				this.paintVerticalSelectedPart(g, paintBounds, insets, parentPath);
+				this.drawingCache.put(parentPath, Boolean.TRUE);
 				parentPath = parentPath.getParentPath();
 			}
 		}
 	}
 
 	protected int getRowForY(int y) {
-		TreePath treePath = getClosestPathForLocation(tree, 0, y);
-		return getRowForPath(tree, treePath);
+		TreePath treePath = this.getClosestPathForLocation(this.tree, 0, y);
+		return this.getRowForPath(this.tree, treePath);
 	}
 
 	protected boolean isLastTreePath(int x, int y) {
+		/*
+		 * Old code y = y - 6; x = x + this.getRightChildIndent();
+		 * 
+		 * TreePath treePath = this.getClosestPathForLocation(this.tree, x, y);
+		 * TreeNode treeNode = (TreeNode) treePath.getLastPathComponent();
+		 * 
+		 * if (treeNode.getParent() == null) { return false; } TreeNode last =
+		 * treeNode.getParent().getChildAt(treeNode.getParent().getChildCount()
+		 * - 1); TreePath lastPath =
+		 * treePath.getParentPath().pathByAddingChild(last);
+		 * 
+		 * Rectangle r = this.getPathBounds(this.tree, lastPath);
+		 * 
+		 * if (r.contains(x, y)) { return true; }
+		 * 
+		 * return false;
+		 */
+
 		y = y - 6;
-		x = x + getRightChildIndent();
-
-		TreePath treePath = getClosestPathForLocation(tree, x, y);
-		TreeNode treeNode = (TreeNode) treePath.getLastPathComponent();
-
-		if (treeNode.getParent() == null)
+		x = x + this.getRightChildIndent();
+		TreePath treePath = this.getClosestPathForLocation(this.tree, x, y);
+		if (treePath.getParentPath() == null) {
 			return false;
-		TreeNode last = treeNode.getParent().getChildAt(
-				treeNode.getParent().getChildCount() - 1);
+		}
+		Object last = this.treeModel.getChild(treePath.getParentPath().getLastPathComponent(), this.treeModel.getChildCount(treePath.getParentPath().getLastPathComponent()) - 1);
 		TreePath lastPath = treePath.getParentPath().pathByAddingChild(last);
-
-		Rectangle r = getPathBounds(tree, lastPath);
-
+		Rectangle r = this.getPathBounds(this.tree, lastPath);
 		if (r.contains(x, y)) {
 			return true;
 		}
-
 		return false;
+
 	}
 
 	protected boolean isSelectedPath(TreePath currentPath) {
-		if (tree != null) {
-			TreePath[] paths = tree.getSelectionPaths();
-			if (paths != null && paths.length > 0 && currentPath != null) {
+		if (this.tree != null) {
+			TreePath[] paths = this.tree.getSelectionPaths();
+			if ((paths != null) && (paths.length > 0) && (currentPath != null)) {
 				for (int i = 0; i < paths.length; i++) {
 					TreePath path = paths[i];
 					if (currentPath.isDescendant(path)) {
@@ -146,84 +160,82 @@ public class OTreeUI extends BasicTreeUI {
 	}
 
 	@Override
-	protected void paintHorizontalPartOfLeg(Graphics g, Rectangle clipBounds,
-			Insets insets, Rectangle bounds, TreePath path, int row,
-			boolean isExpanded, boolean hasBeenExpanded, boolean isLeaf) {
+	protected void paintHorizontalPartOfLeg(Graphics g, Rectangle clipBounds, Insets insets, Rectangle bounds, TreePath path, int row, boolean isExpanded, boolean hasBeenExpanded,
+			boolean isLeaf) {
 
-		Color oldHashColor = getHashColor();
+		Color oldHashColor = this.getHashColor();
 
 		boolean seleted = false;
-		if (isSelectedPath(path)) {
+		if (this.isSelectedPath(path)) {
 			seleted = true;
-			setHashColor(lineSelectionColor);
+			this.setHashColor(this.lineSelectionColor);
 		} else {
-			setHashColor(lineColor);
+			this.setHashColor(this.lineColor);
 		}
 
-		super.paintHorizontalPartOfLeg(g, clipBounds, insets, bounds, path,
-				row, isExpanded, hasBeenExpanded, isLeaf);
+		super.paintHorizontalPartOfLeg(g, clipBounds, insets, bounds, path, row, isExpanded, hasBeenExpanded, isLeaf);
 
-		if (path.getParentPath() != null && seleted) {
-//			paintVerticalSelectedPart(g, clipBounds, insets, path);
+		if ((path.getParentPath() != null) && seleted) {
+			// paintVerticalSelectedPart(g, clipBounds, insets, path);
 		}
 
-		setHashColor(oldHashColor);
+		this.setHashColor(oldHashColor);
 	}
 
-	protected void paintVerticalSelectedPart(Graphics g, Rectangle clipBounds,
-			Insets insets, TreePath selectedPath) {
+	protected void paintVerticalSelectedPart(Graphics g, Rectangle clipBounds, Insets insets, TreePath selectedPath) {
 		TreePath path = selectedPath.getParentPath();
 
-		if (path==null) return;
-		int depth = path.getPathCount() - 1;
-		if (depth == 0 && !getShowsRootHandles() && !isRootVisible()) {
+		if (path == null) {
 			return;
 		}
-		int lineX = getRowX(-1, depth + 1);
-		if (tree.getComponentOrientation().isLeftToRight()) {
-			lineX = lineX - getRightChildIndent() + insets.left;
+		int depth = path.getPathCount() - 1;
+		if ((depth == 0) && !this.getShowsRootHandles() && !this.isRootVisible()) {
+			return;
+		}
+		int lineX = this.getRowX(-1, depth + 1);
+		if (this.tree.getComponentOrientation().isLeftToRight()) {
+			lineX = (lineX - this.getRightChildIndent()) + insets.left;
 		} else {
-			lineX = tree.getWidth() - lineX - insets.right
-					+ getRightChildIndent() - 1;
+			lineX = ((this.tree.getWidth() - lineX - insets.right) + this.getRightChildIndent()) - 1;
 		}
 
 		int clipLeft = clipBounds.x;
 		int clipRight = clipBounds.x + (clipBounds.width - 1);
 
-		if (lineX >= clipLeft && lineX <= clipRight) {
+		if ((lineX >= clipLeft) && (lineX <= clipRight)) {
 			int clipTop = clipBounds.y;
 			int clipBottom = clipBounds.y + clipBounds.height;
-			Rectangle parentBounds = getPathBounds(tree, path);
+			Rectangle parentBounds = this.getPathBounds(this.tree, path);
 
-			Rectangle lastChildBounds = getPathBounds(tree, selectedPath);
+			Rectangle lastChildBounds = this.getPathBounds(this.tree, selectedPath);
 			// getLastChildPath(selectedPath));
 
-			if (lastChildBounds == null)
+			if (lastChildBounds == null) {
 				// This shouldn't happen, but if the model is modified
 				// in another thread it is possible for this to happen.
 				// Swing isn't multithreaded, but I'll add this check in
 				// anyway.
 				return;
+			}
 
 			int top;
 
 			if (parentBounds == null) {
-				top = Math.max(insets.top + getVerticalLegBuffer(), clipTop);
-			} else
-				top = Math.max(parentBounds.y + parentBounds.height
-						+ getVerticalLegBuffer(), clipTop);
-			if (depth == 0 && !isRootVisible()) {
-				TreeModel model = getModel();
+				top = Math.max(insets.top + this.getVerticalLegBuffer(), clipTop);
+			} else {
+				top = Math.max(parentBounds.y + parentBounds.height + this.getVerticalLegBuffer(), clipTop);
+			}
+			if ((depth == 0) && !this.isRootVisible()) {
+				TreeModel model = this.getModel();
 
 				if (model != null) {
 					Object root = model.getRoot();
 
 					if (model.getChildCount(root) > 0) {
-						parentBounds = getPathBounds(tree, path
-								.pathByAddingChild(model.getChild(root, 0)));
-						if (parentBounds != null)
-							top = Math.max(insets.top + getVerticalLegBuffer(),
-									parentBounds.y + parentBounds.height / 2);
+						parentBounds = this.getPathBounds(this.tree, path.pathByAddingChild(model.getChild(root, 0)));
+						if (parentBounds != null) {
+							top = Math.max(insets.top + this.getVerticalLegBuffer(), parentBounds.y + (parentBounds.height / 2));
+						}
 					}
 				}
 			}
@@ -231,34 +243,33 @@ public class OTreeUI extends BasicTreeUI {
 			int bottom = Math.min(lastChildBounds.y + 2, clipBottom);
 
 			if (top <= bottom) {
-				paintVerticalLine(g, tree, lineX, top, bottom, true);
+				this.paintVerticalLine(g, this.tree, lineX, top, bottom, true);
 			}
 		}
 	}
-	
-	protected void paintVerticalLine(Graphics g, JComponent c, int x, int top,
-			int bottom) {
-		paintVerticalLine(g, c, x, top, bottom, false);
+
+	@Override
+	protected void paintVerticalLine(Graphics g, JComponent c, int x, int top, int bottom) {
+		this.paintVerticalLine(g, c, x, top, bottom, false);
 	}
 
 	/**
 	 * Paints a vertical line.
 	 */
-	protected void paintVerticalLine(Graphics g, JComponent c, int x, int top,
-			int bottom, boolean selected) {
+	protected void paintVerticalLine(Graphics g, JComponent c, int x, int top, int bottom, boolean selected) {
 		boolean end = false;
 
-		if (lineTypeCurved) {
+		if (this.lineTypeCurved) {
 			Color old = g.getColor();
 
-			if (isLastTreePath(x, bottom)) {
+			if (this.isLastTreePath(x, bottom)) {
 				end = true;
 			}
 
 			if (selected) {
-				g.setColor(lineSelectionColor);
+				g.setColor(this.lineSelectionColor);
 			} else {
-				g.setColor(lineColor);
+				g.setColor(this.lineColor);
 			}
 			if (end) {
 				g.fillRect(x - 6, top, 2, bottom - top - 6);
@@ -274,14 +285,13 @@ public class OTreeUI extends BasicTreeUI {
 	/**
 	 * Paints a horizontal line.
 	 */
-	protected void paintHorizontalLine(Graphics g, JComponent c, int y,
-			int left, int right) {
-		if (lineTypeCurved) {
+	@Override
+	protected void paintHorizontalLine(Graphics g, JComponent c, int y, int left, int right) {
+		if (this.lineTypeCurved) {
 			// Color old = g.getColor();
 			Paint previousPaint = ((Graphics2D) g).getPaint();
 			RenderingHints rh = ((Graphics2D) g).getRenderingHints();
-			((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-					RenderingHints.VALUE_ANTIALIAS_ON);
+			((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 			// int currentRow = getRowForY(y);
 			// int currentRow = -1;
@@ -312,49 +322,53 @@ public class OTreeUI extends BasicTreeUI {
 	 * Paints the renderer part of a row. The receiver should NOT modify
 	 * <code>clipBounds</code>, or <code>insets</code>.
 	 */
-	protected void paintRow(Graphics g, Rectangle clipBounds, Insets insets,
-			Rectangle bounds, TreePath path, int row, boolean isExpanded,
-			boolean hasBeenExpanded, boolean isLeaf) {
+	@Override
+	protected void paintRow(Graphics g, Rectangle clipBounds, Insets insets, Rectangle bounds, TreePath path, int row, boolean isExpanded, boolean hasBeenExpanded, boolean isLeaf) {
 		// Don't paint the renderer if editing this row.
-		if (editingComponent != null && editingRow == row)
+		if ((this.editingComponent != null) && (this.editingRow == row)) {
 			return;
+		}
 
 		int leadIndex;
 
-		if (tree.hasFocus()) {
-			leadIndex = getLeadSelectionRow();
-		} else
+		if (this.tree.hasFocus()) {
+			leadIndex = this.getLeadSelectionRow();
+		} else {
 			leadIndex = -1;
+		}
 
 		Component component;
 
-		component = currentCellRenderer.getTreeCellRendererComponent(tree, path
-				.getLastPathComponent(), tree.isRowSelected(row), isExpanded,
-				isLeaf, row, (leadIndex == row));
+		component = this.currentCellRenderer.getTreeCellRendererComponent(this.tree, path.getLastPathComponent(), this.tree.isRowSelected(row), isExpanded, isLeaf, row,
+				(leadIndex == row));
 
 		try {
 			/*
-			 * Due to DefaultTreeCellRenderer constructor (the component class) modification into Java7, it is
-			 * necessary to call updateUI to reinitialize the value of 'Tree.rendererFillBackground' property
+			 * Due to DefaultTreeCellRenderer constructor (the component class)
+			 * modification into Java7, it is necessary to call updateUI to
+			 * reinitialize the value of 'Tree.rendererFillBackground' property
 			 * to make that background label was not painted.
 			 */
-			//if(UIManager.getLookAndFeel()  instanceof javax.swing.plaf.nimbus.NimbusLookAndFeel)	{
+			// if(UIManager.getLookAndFeel() instanceof
+			// javax.swing.plaf.nimbus.NimbusLookAndFeel) {
 			String s = System.getProperty("java.version");
-			if(s!=null && s.startsWith("1.7")){
-				((JComponent)component).updateUI();
+			if ((s != null) && s.startsWith("1.7")) {
+				((JComponent) component).updateUI();
 			}
-		} catch (Throwable e) {
-		}
-		rendererPane.paintComponent(g, component, tree, bounds.x, bounds.y,
-				tree.getWidth() - bounds.x, bounds.height, true);
+		} catch (Throwable e) {}
+		this.rendererPane.paintComponent(g, component, this.tree, bounds.x, bounds.y, this.tree.getWidth() - bounds.x, bounds.height, true);
 	}
-	
+
 	@Override
 	public void uninstallUI(JComponent c) {
-		if(tree!=null) tree.setCellRenderer(null);
-		if(tree!=null) tree.setCellEditor(null);
+		if (this.tree != null) {
+			this.tree.setCellRenderer(null);
+		}
+		if (this.tree != null) {
+			this.tree.setCellEditor(null);
+		}
 		super.uninstallUI(c);
-		
+
 	}
-	
+
 }
